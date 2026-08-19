7e7da527541bd49796905ba4cf56a521423901a8
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-19 23:06:03 +0430

    i add the css file also

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import './App.css';
+import Goal from "./components/Goal";
+
 const App = () =>  {
     // return React.createElement("h1" , {} , "hi i am developer");
-    return <h2 title="this work">
-      hi i am react developer
-    </h2>
+    return <div>
+      <h2>what we do</h2>
+     <Goal />
+    </div>
 };
 
 export default App;
---
+++ src/components/Goal.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import './Goal.css';
+
+const Goal = () => {
+    return (
+        <div>
+             <ul >
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+        </div>
+    );
+};
+
+export default Goal;

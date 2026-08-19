df72113ee636560d8b1e1b85ee53766fab23cb7e
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-19 22:53:34 +0430

    i add new change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>hello world</h2>
-    </div>
-  );
-}
+const App = () =>  {
+    // return React.createElement("h1" , {} , "hi i am developer");
+    return <h2 title="this work">
+      hi i am react developer
+    </h2>
+};
 
 export default App;

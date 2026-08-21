52e3119d3744466dc407ac89644386faa13de83e
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-21 18:42:02 +0430

    i add the style code useritem

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import UserList from '../components/UserList';
 
-import User from '../data/users.json';
+// import User from '../data/users.json';
 
 const Users = () => {
-    // const Users = [
-    //     {
-    //         id : "u1",
-    //         name : "alawoddin",
-    //         image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-ab4tAB79y8BEUHWVk7mWAbSH1EStX3WSW8mo2m3Ogw&s=10",
-    //         place : 3
-    //     }
-    // ]
+    const Users = [
+        {
+            id : "u1",
+            name : "alawoddin",
+            image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-ab4tAB79y8BEUHWVk7mWAbSH1EStX3WSW8mo2m3Ogw&s=10",
+            place : 3
+        }
+    ]
     return (
         <div>
-            <UserList items={User} />
+            <UserList items={Users} />
+            {/* <UserList items={User} /> */}
         </div>
     );
 };

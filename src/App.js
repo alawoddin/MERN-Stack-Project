80b0c9f23aea9a4d7560b4c74fd5fd38149f530e
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-21 17:40:59 +0430

    i add the new page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import "./App.css";
 
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./user/pages/User";
 
-
-const App = () =>  {
- 
-    return <div>
-      <h2>Let Start</h2>
-      
-    </div>
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;

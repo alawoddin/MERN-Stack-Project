a170f034b75de5bfac10c4f4a95d307e4fbbb6d3
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-21 22:37:12 +0430

    i add the new folder

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,11 +1,14 @@
 import React from "react";
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
 
 const UserList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
+        <Card>
         <h2>User Not Found...</h2>
+        </Card>
       </div>
     );
   }

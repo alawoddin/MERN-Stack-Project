dc72b7f96c88cb49db0fe7566de7e6aaf1f4ac0c
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-21 22:52:18 +0430

    i add the css style also

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
-import MainNavigation from './shared/components/Navigation/MainNavigation';
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <MainNavigation />
-          {/* <Route path="/" element={<User />} /> */}
-        <Route path="/" element={<Users />} />
-        <Route path="/new/place" element={<NewPlace />} />
-        
-      </Routes>
+      <MainNavigation />
 
+      <main>
+        <Routes>
+          {/* <Route path="/" element={<User />} /> */}
+          <Route path="/" element={<Users />} />
+          <Route path="/new/place" element={<NewPlace />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };

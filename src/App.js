c9f56395cc13795e270f722663a3ffcf9978884a
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-21 17:55:48 +0430

    i add the new route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<User />} /> */}
+          {/* <Route path="/" element={<User />} /> */}
         <Route path="/" element={<Users />} />
+        <Route path="/new/place" element={<NewPlace />} />
+        
       </Routes>
 
     </BrowserRouter>
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewPlace = () => {
+    return (
+        <div>
+            <h2>hello world</h2>
+        </div>
+    );
+};
+
+export default NewPlace;

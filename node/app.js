996e891f1562c5ffb1eefd70a38616f8b580c983
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-25 16:22:41 +0430

    i writing first node js code

--- node/app.js
+++ node/app.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+
 const userName = "ali";
+
+fs.writeFile('user-file.txt' , "name : " +  userName , (err) =>  {
+
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log("Read the file");
+
+})
 
 console.log(userName)

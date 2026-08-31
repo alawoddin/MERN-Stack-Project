73009f82fcd0a7045c8a9813796e069247f956ca
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-08-31 16:58:41 +0430

    i create the express first app

--- node/app.js
+++ node/app.js
@@ -2,6 +2,31 @@ const express  = require("express");
 
 const app = express();
 
+app.use((req , res , next) => {
+  // console.log("what is show ");
+  // next();
+  let body  = '';
+  req.on('end' , () => {
+    const username = body.split('=')[1];
+    if(username) {
+      req.body = {name: username};
+    }
+    next();
+  });
+  req.on('data' , chunk => {
+    body += chunk;
+  });
+});
+
+
+
+app.use((req , res , next) => {
+  if(req.body) {
+    return res.send('<h1>' + req.body.name +  '</h1>');
+  }
+  res.send(`<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></input></form>`)
+});
+
 
 app.listen(5000)
 
